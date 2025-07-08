4ebe6b0c5c8f14ffbfd3477cd1e9c430a7166674
caues <caues@SCHMITZNOT>
2025-07-07 23:16:32 -0300

    Implementado HomeController e ASP.NET MVC

---
+++ GestaoDeEquipamentos.WebApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GestaoDeEquipamentos.WebApp;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews(); // ASP.NET MVC - Model View Controller
+        WebApplication app = builder.Build();
+        app.UseRouting();
+        app.MapDefaultControllerRoute();
+        app.Run();
+    }
+}
