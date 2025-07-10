fafe18a225bc48d7313fab2b43265988cb886818
caues <caues@SCHMITZNOT>
2025-07-10 19:13:26 -0300

    implementado a pasta chamado e equipamento de views

--- GestaoDeEquipamentos.WebApp/Controllers/EquipamentoController.cs
+++ GestaoDeEquipamentos.WebApp/Controllers/EquipamentoController.cs
@@ -1,41 +1,41 @@
-﻿//using GestaoDeEquipamentos.Dominio.ModuloEquipamento;
-//using GestaoDeEquipamentos.Infraestrutura.Arquivos.Compartilhado;
+﻿using GestaoDeEquipamentos.Dominio.ModuloEquipamento;
+using GestaoDeEquipamentos.Infraestrutura.Arquivos.Compartilhado;
 //using GestaoDeEquipamentos.Infraestrutura.Arquivos.ModuloChamado;
-//using GestaoDeEquipamentos.Infraestrutura.Arquivos.ModuloEquipamento;
-//using Microsoft.AspNetCore.Mvc;
+using GestaoDeEquipamentos.Infraestrutura.Arquivos.ModuloEquipamento;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace GestaoDeEquipamentos.WebApp.Controllers;
+namespace GestaoDeEquipamentos.WebApp.Controllers;
 
-//public class EquipamentoController : Controller
-//{
-//    private RepositorioEquipamentoEmArquivo repositorioEquipamento;
+public class EquipamentoController : Controller
+{
+    private RepositorioEquipamentoEmArquivo repositorioEquipamento;
 
-//    public EquipamentoController()
-//    {
-//        ContextoDados contexto = new ContextoDados(true);
-//        repositorioEquipamento = new RepositorioEquipamentoEmArquivo(contexto);
-//    }
+    public EquipamentoController()
+    {
+        ContextoDados contexto = new ContextoDados(true);
+        repositorioEquipamento = new RepositorioEquipamentoEmArquivo(contexto);
+    }
 
-//    public IActionResult Index()
-//    {
-//        List<Equipamento> equipamentos = repositorioEquipamento.SelecionarRegistros();
+    public IActionResult Index()
+    {
+        List<Equipamento> equipamentos = repositorioEquipamento.SelecionarRegistros();
 
-//        return View(equipamentos);
-//    }
+        return View(equipamentos);
+    }
 
-//    [HttpGet]
-//    public IActionResult Cadastrar()
-//    {
-//        return View();
-//    }
+    [HttpGet]
+    public IActionResult Cadastrar()
+    {
+        return View();
+    }
 
-//    [HttpPost]
-//    public IActionResult Cadastrar(string nome, string email, string telefone)
-//    {
-//        Equipamento novoEquipamento = new Equipamento(nome, email, telefone);
+    [HttpPost]
+    public IActionResult Cadastrar(string nome, string email, string telefone)
+    {
+        Equipamento novoEquipamento = new Equipamento(nome, email, telefone);
 
-//        repositorioEquipamento.CadastrarRegistro(novoEquipamento);
+        repositorioEquipamento.CadastrarRegistro(novoEquipamento);
 
-//        return RedirectToAction("Index");
-//    }
-//}
+        return RedirectToAction("Index");
+    }
+}
